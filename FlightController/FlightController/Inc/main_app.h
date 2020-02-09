a99669fae322e8db44477c3f2377a314c070d1d3
lyf44 <luyunfan44@gmail.com>
2020-02-09 17:22:34 +0800

    <FCV2> Deleted old code for STM32F429 nucleo
    Changes:
    1. As above
    
    Test & Result:
    No Error

--- FlightController/FlightController/Inc/main_app.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MAIN_APP_H
-#define _MAIN_APP_H
-
-void MainApp();
-
-// void OnCoreTimerTick();
-
-#endif
--- FlightController/FlightController/Inc/util.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LIB_UTIL_H_
-#define _LIB_UTIL_H_
-
-float Util_Constrain(float inVal, float inMin, float inMax, float outMin, float outMax);
-int Util_Constrain(int inVal, int inMin, int inMax, int outMin, int outMax);
-#endif
